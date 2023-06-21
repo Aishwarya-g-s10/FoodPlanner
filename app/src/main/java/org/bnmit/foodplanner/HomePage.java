87e78c57bb560eb6192cf2494f4dc7c5191ca846
Amrutha21A <amrutha.amar21@gmail.com>
2023-06-22 00:09:43 +0530

    Added icons and empty files for the modules

--- app/src/main/java/org/bnmit/foodplanner/HomePage.java
+++ app/src/main/java/org/bnmit/foodplanner/HomePage.java
@@ -2,13 +2,21 @@ package org.bnmit.foodplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HomePage extends AppCompatActivity {
-
+    ImageView recipe,schedule,timer,shopping,profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        recipe=findViewById(R.id.imgrecipe);
+        schedule=findViewById(R.id.imgschedule);
+        timer=findViewById(R.id.imgtimer);
+        shopping=findViewById(R.id.imgshopping);
+        profile=findViewById(R.id.imguser);
     }
 }
